6e093977bec5325c27668a59e0d5a7c2eb2b7110
sreayan <sreayan69@gmail.com>
2025-03-17 15:07:10 +0530

    class+inMemory

---
+++ src/store/Store.ts
@@ -0,0 +1,31 @@
+export type UserId=string
+
+
+export interface Chat{
+    id:string,
+    userId:UserId
+    name:string,
+    message:string,
+    upvotes:UserId[]
+}
+export abstract class Store{
+    constructor(){
+
+    }
+
+    initRoom(roomId:string){
+
+    }
+
+    getChats(room:string,limit:number,offset:number){
+
+    }
+
+    addChat(userId:UserId,room:string,limit:number,offset:number,name:string,message:string){
+
+    }
+
+    upvote(userId:UserId,room:string,chatId:string){
+         
+    }
+}
